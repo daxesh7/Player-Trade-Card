e3f3fa1d9cb082bf9614b1ea89f0e7c1f413e229
daxesh7 <mehradaxsh7@gmail.com>
2021-11-28 03:51:30 -0600

    init page

---
+++ src/app/services/util.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UtilService {
+
+  constructor() { }
+
+  isNullOrEmpty(value : string) : boolean {
+    return !value || value === '';
+  }
+
+  isNaN(value : string | number) : boolean {
+    return isNaN(Number(value)) ? true : false
+  }
+
+  compareWithNoCaseSensitive(value1 : string | number , value2 : string | number) : boolean {
+    return value1.toString().trim().toLowerCase() === value2.toString().trim().toLowerCase();
+  }
+
+}

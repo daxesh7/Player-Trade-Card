d0ca9de125501dff93e7ba55e438603425d92efb
daxesh7 <mehradaxsh7@gmail.com>
2021-11-29 02:33:35 -0600

    form test cases

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,9 +1,14 @@
+//Core DI
 import { Component, OnInit } from '@angular/core';
 import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+// Store Action Selectors
 import { Store } from '@ngrx/store';
 import * as actions from '../store/actions/player.action';
+import * as selectors from '../store/selectors/player.selector';
 import { AppState } from '../store/state/app.state';
+// Services and Models
 import { UtilService } from '../services/util.service';
+import { initPlayerCard, IPlayerCard } from '../models/player.model';
 
 @Component({
   selector: 'app-home-page',
@@ -13,6 +18,8 @@ import { UtilService } from '../services/util.service';
 export class HomePageComponent implements OnInit {
 
   playerId : number = -1;
+  playerCards : IPlayerCard[] = [];
+  selectedPlayerCard : IPlayerCard = initPlayerCard;
 
   constructor(
     private store : Store<AppState>,
@@ -23,7 +30,9 @@ export class HomePageComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    // get all cards
     this.store.dispatch(actions.getPlayerCards());
+    // based on parma do get by Id Call
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
       const id = paramMap.get('id');      
       if(!this.utilService.isNaN(Number(id))) {
--- src/app/models/player.model.ts
+++ src/app/models/player.model.ts
@@ -3,7 +3,7 @@ export interface IPlayerCard {
     firstName : string;
     lastName: string;
     teamName: string;
-    cardValue ?: number;
+    cardValue : number;
 }
 
 export const initPlayerCard : IPlayerCard = {
@@ -11,11 +11,12 @@ export const initPlayerCard : IPlayerCard = {
     firstName : '',
     lastName: '',
     teamName: '',
+    cardValue : 0
 }
 
 // export const initPlayerCardList : IPlayerCard[] = [];
 export const initPlayerCardList : IPlayerCard[] = [
-    {firstName:' Ike' , lastName:'Anigbogu' , playerNumber: 14 , teamName: 'Indiana Pacers'},
-    {firstName:' Ron' , lastName:'Baker' , playerNumber: 47 , teamName: 'Knicks'},
+    {firstName:' Ike' , lastName:'Anigbogu' , playerNumber: 14 , teamName: 'Indiana Pacers' , cardValue : 0},
+    {firstName:' Ron' , lastName:'Baker' , playerNumber: 47 , teamName: 'Knicks' , cardValue : 0},
     {firstName:' Jabari' , lastName:'Bird' , playerNumber: 25 , teamName: 'Boston Celtics' , cardValue: 2}
 ];

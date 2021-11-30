51a784940d4bbbbdc05faa80d46b30d0de801547
daxesh7 <mehradaxsh7@gmail.com>
2021-11-29 20:30:15 -0600

    service test case

--- src/app/services/player-card.service.spec.ts
+++ src/app/services/player-card.service.spec.ts
@@ -1,8 +1,9 @@
 import { TestBed } from '@angular/core/testing';
+import { IPlayerCard } from '../models/player.model';
 
 import { PlayerCardService } from './player-card.service';
 
-describe('PlayerCardService', () => {
+fdescribe('PlayerCardService', () => {
   let service: PlayerCardService;
 
   beforeEach(() => {
@@ -13,4 +14,31 @@ describe('PlayerCardService', () => {
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  describe('Add Player Card', () => {
+    let addOutput : IPlayerCard [] = [];
+    beforeEach(()=>{
+      service = new PlayerCardService();
+      const list : IPlayerCard[] = [];
+      const itemToAdd : IPlayerCard = {
+        id: '0',
+        firstName: 'test',
+        lastName: 'test',
+        teamName : 'test',
+        playerNumber: 12,
+        cardValue : 0
+      };
+      addOutput = service.addToPlayerCardToList(list , itemToAdd);
+    });
+
+    it('Should Add Player Card', () => {
+      expect(addOutput.length).toEqual(1);
+    });
+
+    it('Should Change Id before Adding', () => {
+      expect(addOutput[0].id).not.toEqual('0');
+    });   
+    
+  })
+
 });
